6518a99c2f76e5f8b2921fe595baf0121f74a6c6
Diego M. Rodrigues <diegomr86@gmail.com>
2013-10-07 15:32:32 -0300

    .

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -7,6 +7,6 @@ end
 
 sitemap :artists do
   JSON.parse(open("http://www.vagalume.com.br/api/rank.php?type=art&period=month&scope=translations&limit=1000").read)['art']['month']['all'].each do |artist|
-     url artist_index_url(artist: artist['name']), last_mod: Time.now, change_freq: "daily", priority: 1.0
+     url artist_root_url(artist['name']), last_mod: Time.now, change_freq: "daily", priority: 1.0
   end
 end
