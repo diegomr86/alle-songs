1c8c1fe3f51337bec9a0292ffefba67c4b0791b8
Diego M. Rodrigues <diegomr86@gmail.com>
2014-09-25 18:51:41 -0300

    .

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,6 @@ Music::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.assets.initialize_on_precompile = true
 end
