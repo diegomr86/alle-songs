10f8b0148f8eefb23824c365aef9564dfe5cb7d1
Diego M. Rodrigues <diegomr86@gmail.com>
2014-10-01 10:47:45 -0300

    .

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,4 +1,5 @@
 require 'open-uri'
+
 host "www.allesongs.com"
 
 sitemap :site do
@@ -6,7 +7,17 @@ sitemap :site do
 end
 
 sitemap :artists do
-  JSON.parse(open("http://www.vagalume.com.br/api/rank.php?type=art&period=month&scope=translations&limit=1000").read)['art']['month']['all'].each do |artist|
-     url artist_root_url(artist['name'].gsub("&", "e").gsub("/", "+").gsub(' ', '+')), last_mod: Time.now, change_freq: "daily", priority: 1.0
+  Artist.find_each do |artist|
+    url artist_root_url(URI.escape(artist.name.gsub("&", "e").gsub("/", "+").gsub('.', ' '))), last_mod: Time.now, change_freq: "daily", priority: 1.0
   end
+end
+
+def artist_custom_path(artist_name)
+  artist_root_path(custom_artist_name(artist_name))
+end
+def artist_custom_url(artist_name)
+  artist_root_url(custom_artist_name(artist_name))
+end
+def custom_artist_name(artist_name)
+  URI.escape(artist_name.gsub("&", "e").gsub("/", "+").gsub('.', ' '))
 end
