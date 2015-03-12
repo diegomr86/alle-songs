19e7ddebb1f45f0a14d473f522f0dbbb62bdb8f0
Diego M. Rodrigues <diegomr86@gmail.com>
2015-03-12 20:53:48 -0300

    .

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -2,11 +2,18 @@ require 'open-uri'
 
 host "www.allesongs.com"
 
+Rockstar.lastfm = {:api_key => "534b78cd8e40fe67e31b50b9693ae512", :api_secret => "abc7b7049ad4e653783023075ca14e73"}
+
 sitemap :site do
   url root_url, last_mod: Time.now, change_freq: "daily", priority: 1.0
 end
 
 sitemap :artists do
+  (1...5).each do |i|
+    Rockstar::Chart.get_instance("chart.getTopArtists", :artists, :artist, {limit: 1000, page: i}, false).each do |artist|
+      url artist_root_url(URI.escape(artist.name.gsub("&", "e").gsub("/", "+").gsub('.', ' '))), last_mod: Time.now, change_freq: "daily", priority: 1.0
+    end
+  end
   Artist.find_each do |artist|
     url artist_root_url(URI.escape(artist.name.gsub("&", "e").gsub("/", "+").gsub('.', ' '))), last_mod: Time.now, change_freq: "daily", priority: 1.0
   end
@@ -19,7 +26,7 @@ sitemap :albums do
 end
 
 sitemap :tags do
-  Rockstar::Tag.top_tags.each do |tag|
+  Rockstar::Tag.get_instance("tag.getTopTags", :top_tags, :tag, {limit: 1000}, false).each do |tag|
     url tag_url(URI.escape(tag.name.gsub("&", "e").gsub("/", "+").gsub('.', ' '))), last_mod: Time.now, change_freq: "daily", priority: 1.0
   end
 end
