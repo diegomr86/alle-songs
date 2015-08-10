d704b336eb365aeceae36bdfae63b95918c1131f
Diego M. Rodrigues <diegomr86@gmail.com>
2015-08-10 13:31:29 -0300

    Configura local_env

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ server '104.236.64.205', port: 22, roles: [:web, :app, :db], primary: true
 
 set :repo_url,        'git@bitbucket.org:diegomr86/alle-songs.git'
 set :application,     'alle-songs'
-set :user,            'root'
+set :user,            'diego'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
 
@@ -12,7 +12,7 @@ set :pty,             true
 set :use_sudo,        false
 set :stage,           :production
 set :deploy_via,      :remote_cache
-set :deploy_to,       "/#{fetch(:user)}/apps/#{fetch(:application)}"
+set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
