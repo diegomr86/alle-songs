6aa8daebacc125aaf730d40100ec86491263e0b2
Diego M. Rodrigues <diegomr86@gmail.com>
2015-08-10 12:01:34 -0300

    Configura local_env

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,11 @@ module Music
     config.before_configuration do
       env_file = File.join(Rails.root, 'config', 'local_env.yml')
       YAML.load(File.open(env_file)).each do |key, value|
+        puts "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+        puts key
+        puts key.class
+        puts key.to_s
+        puts value
         ENV[key.to_s] = value
       end if File.exists?(env_file)
     end
